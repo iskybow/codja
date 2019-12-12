9c41205c70a5d7da14624a7736cf9803eba6c37c
kir_boyko <kir_boyko@bk.ru>
2019-12-12 14:00:06 +0300

    nav

--- src/shared/js/script.js
+++ src/shared/js/script.js
@@ -1,8 +1,9 @@
 class Shared {
-    constructor() {}
+  constructor() {
+  }
 }
 
-$(function() {
+$(function () {
 
   //line show
   function lineShow() {
@@ -12,6 +13,71 @@ $(function() {
       $(this).find('.jsAddDelay').css({'transition-delay': delay + 's'});
     });
   }
+
+  // let last_scroll = 0;
+  // window.onscroll = function (e) {
+  //   e.preventDefault();
+  //   if (window.scrollY > last_scroll) {
+  //     console.log('down');
+  //     console.log(window.scrollY);
+  //     console.log(last_scroll);
+  //     // let b = $('#services').offset().top;
+  //     // $('html').animate({ scrollTop: b }, 1100);
+  //   } else {
+  //     console.log('up');
+  //     console.log(window.scrollY);
+  //     console.log(last_scroll);
+  //   }
+  //   last_scroll = window.scrollY;
+  // };
+
+  // $('html').bind('mousewheel DOMMouseScroll MozMousePixelScroll', function(event) {
+  //   event.preventDefault();
+  //   event.stopPropagation();
+  //   return;
+  //   let delta = parseInt(event.originalEvent.wheelDelta || -event.originalEvent.detail);
+  //   if (delta >= 0) {
+  //     console.log('up');
+  //     // let b = $('#services').offset().top;
+  //     // $('html').animate({ scrollTop: b }, 1100);
+  //   } else {
+  //     console.log('down');
+  //   }
+  //
+  // });
+
+  // $('html').bind("mousewheel DOMMouseScroll MozMousePixelScroll", function (e) {
+  //   // e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+  //   if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
+  //     console.log('up');
+  //     let a = $('#main').offset().top;
+  //     $('html').animate({ scrollTop: a }, 1100);
+  //     // e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+  //   }
+  //   else {
+  //     console.log('down');
+  //     let b = $('#services').offset().top;
+  //     $('html').animate({ scrollTop: b }, 1100);
+  //     // e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+  //   }
+  //   // let b = $('#services').offset().top;
+  //   // $('html').animate({ scrollTop: b }, 1100);
+  // });
+
+  $(window).bind('mousewheel DOMMouseScroll MozMousePixelScroll', function (event) {
+    // event.preventDefault();
+
+    console.log(event.originalEvent.wheelDelta);
+    if (event.originalEvent.wheelDelta >= 0) {
+      console.log('Scroll up');
+      // let a = $('#main').offset().top;
+      // $('html').animate({ scrollTop: a });
+    } else {
+      console.log('Scroll down');
+      // let b = $('#services').offset().top;
+      // $('html').animate({ scrollTop: b });
+    }
+  });
 
   //line hide reverse
   function lineShowReverse(self) {
